73bc8fae7ef140de4a83bb798e8f9efcb3dca365
Candyer <candyer@example.com>
2015-08-23 16:37:29 +0200

    draw the UK flag

--- pixel.py
+++ pixel.py
@@ -26,53 +26,56 @@ def make_shape(func, width=300, height=100):
 
 
 # make it run with an horizontal line
-# def horizontal_line(pixels, width, height, black_pixel):
-# 	"""
-# 	this function will show you a horizontal line
-# 	"""
-# 	for w in range(0, width):
-# 		pixels[w, height / 2] = black_pixel   # "height / 2" -> keep the line in the middle
+def horizontal_line(pixels, width, height, black_pixel):
+	"""
+	this function will show you a horizontal line
+	"""
+	for w in range(0, width):
+		pixels[w, height / 2] = black_pixel   # "height / 2" -> keep the line in the middle
 
 # make_shape(horizontal_line)
 
 
 # make it run with a vertical line
-# def vertical_line(pixels, width, height, black_pixel):
-# 	"""
-# 	this function will show you a vertical line
-# 	"""
-# 	for h in range(0, height):
-# 		pixels[width / 2, h] = black_pixel   # "width / 2" -> keep the line in the middle
+def vertical_line(pixels, width, height, black_pixel):
+	"""
+	this function will show you a vertical line
+	"""
+	for h in range(0, height):
+		pixels[width / 2, h] = black_pixel   # "width / 2" -> keep the line in the middle
 
 # make_shape(vertical_line)
 
 		
 # make it run with a cross
-# def cross(pixels, width, height, black_pixel):
-# 	"""
-# 	this function will show you a cross
-# 	it only works if the image is a square
-# 	"""
-	# for w in range(0, width):
-	# 	for h in range(0, height): 
-	# 		w = h
-	# 		pixels[w, h] = black_pixel	
-	# w = 0
-	# h = 0
-	# while w < width and h < height:
-	# 	pixels[w, h] = black_pixel
-	# 	w = w + 1
-	# 	h = h + 1
+def cross(pixels, width, height, black_pixel):
+	"""
+	this function will show you a cross
+	it only works if the image is a square
+	"""
+	w = 0
+	h = 0
+	while w < width and h < height:
+		pixels[w, h] = black_pixel
+		w = w + 1
+		h = h + 1
 
-	# w = 0
-	# h = 700
-	# while w < width and h >= 0:
-	# 	pixels[w, h-1] = black_pixel
-	# 	w = w + 1
-	# 	h = h - 1
+	w = 0
+	h = height
+	while w < width and h >= 0:
+		pixels[w, h-1] = black_pixel
+		w = w + 1
+		h = h - 1
 
-# make_shape(cross, width=700, height=700)	
+# make_shape(cross, width=900, height=900)	
 
+# make a UK flag
+def uk_flag(pixels, width, height, black_pixel):
+	horizontal_line(pixels, width, height, black_pixel)
+	vertical_line(pixels, width, height, black_pixel)
+	cross(pixels, width, height, black_pixel)
+
+make_shape(uk_flag, width=300, height=300)
 
 # make it run with stripes
 # def stripes(pixels, width, height, black_pixel):
@@ -126,15 +129,53 @@ def make_shape(func, width=300, height=100):
 # 	thick_h = height / 8.0
 # 	for w in range(width):
 # 		for h in range(height):
-# 			if (w // thick_w) % 4 == (h // thick_h) % 4:
+# 			if (w // thick_w) % 2 == (h // thick_h) % 2:
 # 				pixels[w, h] = black_pixel
 
 # make_shape(example_chess)
 
-# make it run with a circle :)
+
+# make it run with a black circle:)
+# def black_circle(pixels, width, height, black_pixel):
+# 	""" this function will show you a circle """
+# 	import math
+# 	if height < width:
+# 		radius = height / 2
+# 	else:
+# 		radius = width / 2
+# 	middle_pixel = [width / 2 , height / 2]
+# 	for h in range(height):
+# 		for w in range(width):	
+# 			distance = math.sqrt((w - width / 2)**2 + (h - height / 2)**2)
+# 			if distance <= radius:
+# 				pixels[w , h] = black_pixel
+
+			
+# make_shape(black_circle)
 
 
-# make a UK flag
+# make it run with an empty circle:)
+# def empty_circle(pixels, width, height, black_pixel):
+# 	""" this function will show you a circle """
+# 	import math
+# 	if height < width:
+# 		radius = height / 2
+# 	else:
+# 		radius = width / 2
+# 	# or radius = min(width, height) / 2 
+# 	for h in range(height):
+# 		for w in range(width):	
+# 			distance = math.sqrt((w - width / 2)**2 + (h - height / 2)**2)
+# 			if abs(distance - radius) <= 1:
+# 				pixels[w , h] = black_pixel
+			
+# make_shape(empty_circle)
+
+
+
+
+
+
 
 
 
