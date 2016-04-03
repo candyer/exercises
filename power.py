ed9234789ecad5310cf51b1fc0365572a5e93b46
candyer <candyer@users.noreply.github.com>
2016-04-03 23:10:25 +0200

    two ways to write power function

--- power.py
+++ power.py
@@ -1,34 +1,30 @@
 # 1
 def dummy_power(n, p):
     """ return n**p"""
-    counter = 0
     if p < 0:
         return 1.0 / dummy_power(n, -p)
     total = 1
-    for _ in range(p):
+    for i in range(p): 
         total *= n
-        counter += 1
-        print "{} operation implemented".format(counter)
+        print "p = {}".format(i+1)
     return total
 print dummy_power(2, 100) 
 # print dummy_power(3,  8) #6561
 # print dummy_power(2, -2) #0.25
 
-
-
 # 2
 def faster_power(n, p):
-    if p < 0:  return 1.0 / dummy_power(n, -p)
+    if p < 0:  return 1.0 / faster_power(n, -p)
     if p == 0: return 1
     if p == 1: return n
     sqr = n**2
     if p % 2:
-        print "p = {} ".format(p)
+        print "p = {}".format(p)
         return n * faster_power(n, p-1)
-    print "p = {} ".format(p)
+    print "p = {}".format(p)
     return faster_power(sqr, p/2)
 
 
-print faster_power(2, 100) 
+# print faster_power(2, 100) 
 # print faster_power(3,  8) #6561
-# print faster_power(2, -2) #0.25
+# print faster_power(2, -10) #0.25
