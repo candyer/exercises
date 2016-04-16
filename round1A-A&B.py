c23868bf22b5e889fb0572664b0c90779e7d71f4
candyer <candyer@users.noreply.github.com>
2016-04-16 18:57:42 +0200

    Round 1A - Google Code Jam 2016

--- round1A-A&B.py
+++ round1A-A&B.py
@@ -1,4 +1,6 @@
 #problem A  The Last Word
+
+#Solution one, O(n^2)
 def solve(s):
     res = s[0]
     for c in s[1:]:
@@ -7,6 +9,16 @@ def solve(s):
         else:
             res = res + c
     return res
+
+#Solution two, O(n)
+def solve(s):
+    left, right = [s[0]], []
+    for c in s[1:]:
+        if c >= left[-1]:
+            left.append(c)
+        else:
+            right.append(c)
+    return ''.join(list(reversed(left)) + right)
 
 def a():
     T = int(raw_input())
@@ -18,16 +30,29 @@ if __name__ == '__main__':
     a()
 
 
+######################################################################################################################################
 #Problem B. Rank and File
+
+# solution one  
 def solve(l):
     new_l = [s for sub in l for s in sub ]
-
     d = {}
     for n in new_l:
         d[n] = d.get(n,0)+1
-
     res = [k for k,v in d.items() if v%2 != 0]
     return " ".join(map(str, sorted(res)))
+
+
+# solution two 
+import collections
+def solve(pieces):
+    count= collections.defaultdict(lambda: 0)
+    for piece in pieces:
+        for num in piece:
+           count[num] += 1
+    res = [k for k,v in count.items() if v%2]
+    return ' '.join(map(str,sorted(res)))
+
 
 def b():
     T = int(raw_input())
